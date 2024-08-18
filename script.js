8b89aadc2399a7ea8c8a356ef9e38130d79c6ac2
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-18 07:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let div = document.createElement("div");
+document.body.append(div);
+div.setAttribute('id', 'display');
+
+// let arr = ['+','-','/','*','(',')','C','back',]
+let boxesID = ['plus','-','divi','*','op','cl','C','back','equal'];
+let boxesName = ['C','/','*','<-',
+				 '7','8','9','-',
+				 '4','5','6','+',
+				 '1','2','3','.',
+					'(','0',')','='
+				]
+let boxesName = ['C','/','*','<-','7','8','9','-','4','5','6','+','1','2','3','.',
+					'(','0',')','='
+				]
+
+for (let i = 1; i <= 20; i++) {
+  let smallDiv = document.createElement("div");
+  div.append(smallDiv);
+  smallDiv.setAttribute("class", `boxes box-${i}`);
+  console.log(smallDiv);
+}
+
+console.log(div);
