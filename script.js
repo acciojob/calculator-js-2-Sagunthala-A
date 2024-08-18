e6b0d9960462bc2f0dd6cd3a2b56853f76d4430c
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-18 07:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 let div = document.createElement("div");
 document.body.append(div);
-div.setAttribute('id', 'display');
+div.setAttribute("id", "display");
+let answer = document.createElement("div");
+div.append(answer);
+answer.setAttribute("id", "answer");
 
 // let arr = ['+','-','/','*','(',')','C','back',]
 // let boxesID = ['plus','-','divi','*','op','cl','C','divi','equal'];
-let boxesId = ['C','divi','*','back',
-				 '7','8','9','-',
-				 '4','5','6','plus',
-				 '1','2','3','.',
-				'op','0','cl','equal'
-				]
-let boxesName = ['C','/','*','<-',
-				 '7','8','9','-',
-				 '4','5','6','+',
-				 '1','2','3','.',
-				'(','0',')','='
-				]
+let boxesId = [
+  "C",
+  "divi",
+  "*",
+  "back",
+  "7",
+  "8",
+  "9",
+  "-",
+  "4",
+  "5",
+  "6",
+  "plus",
+  "1",
+  "2",
+  "3",
+  ".",
+  "op",
+  "0",
+  "cl",
+  "equal",
+];
+let boxesName = [
+  "C",
+  "/",
+  "*",
+  "<-",
+  "7",
+  "8",
+  "9",
+  "-",
+  "4",
+  "5",
+  "6",
+  "+",
+  "1",
+  "2",
+  "3",
+  ".",
+  "(",
+  "0",
+  ")",
+  "=",
+];
 
 for (let i = 0; i < 20; i++) {
   let smallDiv = document.createElement("div");
   div.append(smallDiv);
   smallDiv.setAttribute("id", `${boxesId[i]}`);
-	smallDiv.innerText = `${boxesName[i]}`
+  smallDiv.innerText = `${boxesName[i]}`;
   console.log(smallDiv);
 }
 
 console.log(div);
-
-
-
-
-
-
-
-
-
